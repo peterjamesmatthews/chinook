24245ebdc043c5c76687a23373d99c79cdbdd7f2
Peter Matthews <peter@pjmatthews.dev>
2023-12-31 12:48:09 -0600

    introduce testdata package

---
+++ api/testdata/OpenElseFatal.go
@@ -0,0 +1,20 @@
+package testdata
+
+import (
+	"os"
+	"testing"
+)
+
+// OpenElseFatal gets a *os.File with os.Open and returns it.
+//
+// If an error occurs, the test will fail fatally.
+func OpenElseFatal(t *testing.T, path string) *os.File {
+	t.Helper()
+
+	file, err := os.Open(path)
+	if err != nil {
+		t.Fatalf("failed to open file: %v", err)
+	}
+
+	return file
+}
