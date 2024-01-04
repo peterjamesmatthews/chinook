0a27820ea4043c73629011b6d7830bff4d284678
Peter Matthews <peter@pjmatthews.dev>
2024-01-03 22:43:42 -0600

    test context

--- api/internal/http/context.go
+++ api/internal/http/context.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
 
@@ -28,20 +29,29 @@ func WrapWithChinookInContext(handler http.Handler, chinook *gorm.DB) http.Handl
 	})
 }
 
+var ErrMissingDatabaseInContext = errors.New("missing database in context")
+
+type ErrUnexpectedChinookType struct {
+	val any
+}
+
+func (e ErrUnexpectedChinookType) Error() string {
+	return fmt.Sprintf("expected *gorm.DB in context, got %T", e.val)
+}
+
 // GetChinookFromContext tries to retrieve the chinook database from ctx.
 //
 // # Errors
 //   - ErrMissingDatabaseInContext: if the database is not present in the context
 func GetChinookFromContext(ctx context.Context) (*gorm.DB, error) {
 	val := ctx.Value(chinook)
-	var nilDB *gorm.DB
-	if val == nilDB {
-		return nil, fmt.Errorf("missing database in context")
+	if val == nil {
+		return nil, ErrMissingDatabaseInContext
 	}
 
 	db, ok := val.(*gorm.DB)
 	if !ok {
-		return nil, fmt.Errorf("expected *gorm.DB in context, got %T", val)
+		return nil, &ErrUnexpectedChinookType{val}
 	}
 
 	return db, nil
---
+++ api/internal/http/context_test.go
@@ -0,0 +1,100 @@
+package http
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"gorm.io/gorm"
+)
+
+var testDB *gorm.DB = &gorm.DB{}
+
+func TestGetChinookFromContext(t *testing.T) {
+	tests := []struct {
+		name string
+		ctx  context.Context
+		want *gorm.DB
+		err  error
+	}{
+		{
+			name: "happy path",
+			ctx:  context.WithValue(context.Background(), chinook, testDB),
+			want: testDB,
+			err:  nil,
+		},
+		{
+			name: "missing database in context",
+			ctx:  context.Background(),
+			want: nil,
+			err:  ErrMissingDatabaseInContext,
+		},
+		{
+			name: "unexpected chinook type",
+			ctx:  context.WithValue(context.Background(), chinook, "not a *gorm.DB"),
+			want: nil,
+			err:  &ErrUnexpectedChinookType{"not a *gorm.DB"},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got, err := GetChinookFromContext(test.ctx)
+
+			if got != test.want {
+				t.Errorf("got %v, want %v", got, test.want)
+			}
+
+			if err != nil && err.Error() != test.err.Error() {
+				t.Errorf("got %v, want %v", err, test.err)
+			}
+		})
+	}
+}
+
+func TestHandleGettingChinookFromContext(t *testing.T) {
+	tests := []struct {
+		name     string
+		ctx      context.Context
+		response http.Response
+		want     *gorm.DB
+		err      error
+	}{
+		{
+			name:     "happy path",
+			ctx:      context.WithValue(context.Background(), chinook, testDB),
+			want:     testDB,
+			response: http.Response{},
+			err:      nil,
+		},
+		{
+			name: "failed to get database",
+			ctx:  context.Background(),
+			response: http.Response{
+				StatusCode: http.StatusInternalServerError,
+				Body:       io.NopCloser(strings.NewReader("failed to get database\nmissing database in context")),
+			},
+			want: nil,
+			err:  ErrMissingDatabaseInContext,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			r, _ := http.NewRequestWithContext(test.ctx, http.MethodGet, "/", nil)
+			got, err := handleGettingChinookFromContext(w, r)
+			if got != test.want {
+				t.Errorf("got %v, want %v", got, test.want)
+			}
+			if err != nil && err.Error() != test.err.Error() {
+				t.Errorf("got %v, want %v", err, test.err)
+			}
+			if err = AssertSoftResponseEquality(t, &test.response, w.Result()); err != nil {
+				t.Error(err)
+			}
+		})
+	}
+}
